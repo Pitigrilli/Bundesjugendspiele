555f273ac5df17bb9bd2cec573ff7c8e5044adf0
Claus Behl <behl.claus@jack-steinberger-gymnasium.de>
2017-05-15 16:24:09 +0200

    Initial commit

---
+++ src/modell/Jahrgang.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modell;
+import java.util.ArrayList;
+/**
+ *
+ * @author finckh.stefan
+ */
+public class Jahrgang {
+    ArrayList Klasse = new ArrayList();
+    
+    
+    
+    
+    
+    public double durchschnittErrechnen(){
+        double punktedurchschnitt=0;
+        
+        
+        return punktedurchschnitt;
+    }
+}
---
+++ src/modell/Schueler.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modell;
+
+/**
+ *
+ * @author zehe.jonas
+ */
+public class Schueler {
+        private String name ;
+    private String vorname;
+    private int geburtsjahr;
+    private char geschlecht;
+    private int gesamtpunktzahl;
+    private char urkunde;
+    public Schueler (String name,String vorname,int geburtsjahr,char geschlecht){
+        
+        
+    }
+}
